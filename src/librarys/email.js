e74cd910d90696fd2954c02555bea6acb2a693a6
ugtdbt <ugtdbt@gmail.com>
2019-09-06 09:59:32 +0530

    Remove Email Logger.....

--- src/librarys/email.js
+++ src/librarys/email.js
@@ -1,13 +1,11 @@
 const emailconfig = require('./../config/email.json');
 const nodemailer = require('nodemailer');
-const simplenodelogger = require('simple-node-logger');
 
 let sendmail = async (emailtemplate) => {
 
     try
     {
 
-        const log = simplenodelogger.createSimpleFileLogger('emaillogger.log');
 
         let emailtemplateobject = {
             from: '"'+ process.env.FROM_NAME +'" <'+ process.env.FROM_EMAIL +'>', // sender address
@@ -21,11 +19,9 @@ let sendmail = async (emailtemplate) => {
         let info = await transporter.sendMail(emailtemplateobject);
 
         console.log("Message sent: %s", info.messageId);
-        log.info("Message sent: %s", info.messageId);
 
-        // Preview only available when sending through an Ethereal account
+        // // Preview only available when sending through an Ethereal account
         console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        log.info("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
     }
     catch(e)
